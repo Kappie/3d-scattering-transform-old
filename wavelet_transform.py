689dcad250fadd76e514a8bfa8436971e49c123b
Adam Hilbert <hilbysfe@gmail.com>
2017-09-27 11:23:53 +0200

    scattering transform werkt

--- wavelet_transform.py
+++ wavelet_transform.py
@@ -1,52 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
 from scipy.signal import convolve
 from wavelets import gabor_filter, gaussian_filter
+from collections import namedtuple
 
 # import pywt
 # import pywt.data
 
+Transform = namedtuple("WaveletTransform", ["transform", "js", "alphas", "betas", "gammas", "scattering_coefficient"])
 
-
-
-def wavelet_transform(image, j, alpha, beta, gamma, dimensions=[5, 5, 5]):
-    gab_filter = gabor_filter(j, alpha, beta, gamma, dimensions)
+def wavelet_transform(image, j, alpha, beta, gamma):
+    gab_filter = gabor_filter(j, alpha, beta, gamma)
     result = convolve(image, gab_filter, mode="same")
     return result
 
 def scattering_transform(image, J, alphas, betas, gammas, m):
-    # js: scales, 0, ..., J
-    # alphas, betas, gammas: positive angles
-    # m: order of scattering transform
+    transforms = [[Transform(image, [], [], [], [], gaussian_average(image, J))]]
 
-    transforms = {}
-    coefficients = []
-    for j in range(J):
-        transforms[j] = []
-        for a, alpha in enumerate(alphas):
-            for b, beta in enumerate(betas):
-                for c, gamma in enumerate(gammas):
-                    transform = np.absolute(wavelet_transform(image, j, alpha, beta, gamma))
-                    coefficient = gaussian_average(image, J)
-                    transforms[j].append(transform)
-                    coefficients.append(coefficient)
+    for order in range(m):
+        transforms_previous_layer = transforms[-1]
+        transforms_current_layer = []
+        for transform in transforms_previous_layer:
+            # For the second and higher layers, only scales j < j_max are relevant,
+            # where j_max is the length scale of the previous wavelet transform. (See Bruna 2013.)
+            if transform.js == []:
+                j_max = J
+            else:
+                j_max = transform.js[-1]
 
-    if m > 1:
-        transforms_higher_orders = []
-        coefficients_higher_orders = []
-        for transform in transforms:
-            # TODO: not all js are needed in deeper layers
-            result = scattering_transform(transform, J, alphas, betas, gammas, m-1)
-            transforms_higher_orders.extend(result["transforms"])
-            coefficients_higher_orders.extend(result["coefficients"])
-        transforms.extend(transforms_higher_orders)
-        coefficients.extend(coefficients_higher_orders)
+            for j, alpha, beta, gamma in product(range(j_max), alphas, betas, gammas):
+                next_transform = np.absolute(wavelet_transform(transform.transform, j, alpha, beta, gamma))
+                scattering_coefficient = gaussian_average(next_transform, J)
+                transforms_current_layer.append(Transform(
+                    next_transform,
+                    transform.js + [j],
+                    transform.alphas + [alpha],
+                    transform.betas + [beta],
+                    transform.gammas + [gamma],
+                    scattering_coefficient))
+                print("appended j:{0}, alpha: {1}, beta: {2}, gamma: {3} at order {4}".format(j, alpha, beta, gamma, order))
 
-    return {"transforms": transforms, "coefficients": coefficients}
+        transforms.append(transforms_current_layer)
 
-def gaussian_average(image, J, dimensions=[5, 5, 5]):
-    return np.average( convolve(image, gaussian_filter(J, dimensions)) )
+    return transforms
 
+def gaussian_average(image, J):
+    return np.average( convolve(image, gaussian_filter(J)) )
 
 # number of scales
 J = 2
@@ -57,8 +57,8 @@ m = 2
 # positive angles in uniform directions
 alphas = betas = gammas = [(a/(L - 1))*np.pi for a in range(L)]
 
-random_data = np.random.rand(21, 21, 21)
+random_data = np.random.rand(31, 31, 31)
 
 result = scattering_transform(random_data, J, alphas, betas, gammas, m)
-print(len(result["coefficients"]))
-print(result["coefficients"])
+print(len(result[1]))
+print(len(result[2]))
--- wavelets.py
+++ wavelets.py
@@ -6,7 +6,7 @@ from cube_show_slider import cube_show_slider
 def mother_gabor(u, xi, sigma):
     return np.exp(-np.dot(u, u)/(2*sigma**2) + 1j*np.dot(xi, u))
 
-def gabor_filter(j, alpha, beta, gamma, dimensions, xi=np.array([3*np.pi/4, 0, 0]), sigma=1, a=2):
+def gabor_filter(j, alpha, beta, gamma, max_dimension=int(11), xi=np.array([3*np.pi/4, 0, 0]), sigma=1, a=2):
     """
     xi: modulation of periodic part of wavelet
     sigma: standard deviation of gaussian window
@@ -15,7 +15,7 @@ def gabor_filter(j, alpha, beta, gamma, dimensions, xi=np.array([3*np.pi/4, 0, 0
     # xi = np.array([0, 0, 0])
     rotation_matrix = euler2mat(alpha, beta, gamma, 'sxyz')
 
-    width, height, depth = dimensions
+    width = height = depth = max_dimension
     gab_filter = np.zeros([width, height, depth], dtype=np.complex)
     for x in range(width):
         for y in range(height):
@@ -24,13 +24,26 @@ def gabor_filter(j, alpha, beta, gamma, dimensions, xi=np.array([3*np.pi/4, 0, 0
                 rotated_u = np.dot(rotation_matrix, np.array([centered_x, centered_y, centered_z]))
                 gab_filter[x, y, z] = (a**-j)*(1/sigma)*mother_gabor(rotated_u*a**-j, xi, sigma)
 
-    return gab_filter
+    return crop(gab_filter)
 
-def gaussian_filter(J, dimensions):
-    return np.real( gabor_filter(J, 0, 0, 0, dimensions, xi=np.array([0, 0, 0])) )
+def gaussian_filter(J):
+    return np.real( gabor_filter(J, 0, 0, 0, xi=np.array([0, 0, 0])) )
 
 def gabor_filters(js, alphas, betas, gammas, dimensions):
     return 0
 
 def center(index, list_length):
     return int(np.floor(index - (list_length-1)/2))
+
+def crop(data, lower_threshold=1e-5):
+    """
+    Crops array of complex numbers if absolute value is smaller than lower_threshold
+    """
+    for i in range(data.ndim):
+        data = np.swapaxes(data, 0, i)  # send i-th axis to front
+        while np.all( np.absolute(data)[0]<lower_threshold ):
+            data = data[1:]
+        while np.all( np.absolute(data)[-1]<lower_threshold ):
+            data = data[:-1]
+        data = np.swapaxes(data, 0, i)  # send i-th axis to its original position
+    return data
