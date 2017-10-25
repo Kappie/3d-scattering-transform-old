5a9c449e3a7f65129b3e64c30e1a4931dac0d316
Adam Hilbert <hilbysfe@gmail.com>
2017-10-25 14:38:59 +0200

    backup

--- affine_space_approximation_classifier.py
+++ affine_space_approximation_classifier.py
@@ -1,10 +1,13 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.decomposition import PCA
-from scipy.sparse.linalg import svds
+from scipy.sparse.linalg import svds, eigs
 
 
 class AffineSpaceApproximationClassifier:
+    """
+    See "Scattering Representations for Recognition" (PhD thesis J. Bruna), section 3.4 Generative Classification with Affine models.
+    """
     def __init__(self, d):
         """
         d: dimension of the approximation space.
@@ -16,17 +19,17 @@ class AffineSpaceApproximationClassifier:
         X: {label: (N_label, D) array} dict, where N_label is the number of data points of a certain label and D is the number
         of features.
         """
-        self.labels = X.keys
+        self.labels = list(X.keys())
         self.X = X
 
         eigenvectors = {}
-        singular_values = {}
+        eigenvalues = {}
         centroids = {}
         for label, data in X.items():
-            projectors[label], singular_values[label], centroids[label] = self._projector_onto_subspace(data)
+            eigenvectors[label], eigenvalues[label], centroids[label] = self._projector_onto_subspace(data)
 
         self.eigenvectors = eigenvectors
-        self.singular_values = singular_values
+        self.eigenvalues = eigenvalues
         self.centroids = centroids
 
 
@@ -35,14 +38,26 @@ class AffineSpaceApproximationClassifier:
         labels = np.zeros(n_samples)
         for i in range(n_samples):
             feature_vector = X[i, :]
-            label = self._predict_vector(feature_vector)
+            labels[i] = self._predict_vector(feature_vector)
+        return labels
+
 
     def _predict_vector(self, x):
         approximation_errors = np.zeros(np.size(self.labels))
-        for label in self.labels:
-            
+        for index, label in enumerate(self.labels):
+            approximation_errors[index] = self._approximation_error(x, label)
+
+        return self.labels[np.argmax(approximation_errors)]
 
 
+    def _approximation_error(self, x, label):
+        centroid = self.centroids[label]
+        eigenvectors = self.eigenvectors[label]
+        eigenvalues = self.eigenvalues[label]
+        projector = np.linalg.multi_dot([eigenvectors, np.diag(eigenvalues), eigenvectors.T])
+        x_centered = x - centroid
+        approximation_error = np.linalg.norm(x_centered - np.dot(projector, x_centered))
+        return approximation_error
 
 
     def _projector_onto_subspace(self, X_label):
@@ -52,17 +67,21 @@ class AffineSpaceApproximationClassifier:
         centroid = np.mean(X_label, axis=0)
         # Each row represents an observation
         covariance_matrix = np.cov(X_label, rowvar=False)
-        U, s, U_transpose = svds(covariance_matrix, k=self.d)
-        return U, s, centroid
+        # U, eigenvalues, U_transpose = eigs(covariance_matrix, k=self.d)
+        eigenvalues, eigenvectors = eigs(covariance_matrix, k=self.d)
+        eigenvalues = np.real(eigenvalues)
+        eigenvectors = np.real(eigenvectors)
+        return eigenvectors, eigenvalues, centroid
 
-def separate_dataset(X, labels, labels):
+
+def separate_dataset(X, target_labels, label_names):
     """
     Given labeled dataset X of shape (n_samples, n_features), transform it into
     a dict with label names as keys and corresponding feature vectors as values.
     """
     result = {}
-    for label in labels:
-        indices = labels == label
+    for label in label_names:
+        indices = target_labels == label
         result[label] = X[indices]
     return result
 
@@ -82,7 +101,12 @@ if __name__ == '__main__':
     test_set = feature_vectors[n_training_set:, :]
     test_labels = labels[n_training_set:]
 
-    d = 5
+    d = 2
     classifier = AffineSpaceApproximationClassifier(d)
-    decompositions = classifier.fit(labeled_training_set)
-    print(decompositions[0].get_covariance().shape)
+    classifier.fit(labeled_training_set)
+    predictions = classifier.predict(test_set)
+    correct = predictions == test_labels
+    percentage = np.count_nonzero(correct) / correct.shape[0]
+    print(percentage)
+    print(np.count_nonzero(correct))
+    print(correct.shape)
