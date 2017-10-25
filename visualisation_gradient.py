4bdbbfc03cb26bec2124a4e9884712c04de92211
Adam Hilbert <hilbysfe@gmail.com>
2017-10-25 14:42:24 +0200

    backup

--- visualisation_gradient.py
+++ visualisation_gradient.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+
+def approximate_gradient(classifier, x, X, k, epsilon):
+    """
+    classifier: outputs probability of vector x belonging to a certain class (G(x) in paper)
+    x: input feature vector
+    X: training data set (needed in order to calculate priciple directions)
+    k: number of principle directions to use in approximating the gradient of the classifier G(x).
+    epsilon: magnitude of perturbation (must be small.)
+    """
+    return lalalal
