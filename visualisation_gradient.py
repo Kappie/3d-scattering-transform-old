b4320a40f5b7205fcb99fc1113c1dcfc70c894b4
Adam Hilbert <hilbysfe@gmail.com>
2017-10-27 11:47:50 +0200

    update

--- visualisation_gradient.py
+++ visualisation_gradient.py
@@ -1,7 +1,13 @@
 import numpy as np
+from sklearn import datasets
+from scipy.sparse.linalg import eigs
+from plot_slices import plot2d
+
+# Implements the visualisation method described in Adel et al.
+# 3D scattering transforms for disease classification in neuroimaging (2017).
 
 
-def approximate_gradient(classifier, x, X, k, epsilon):
+def approximate_gradient(classifier, x, X, k, epsilon=1e-5):
     """
     classifier: outputs probability of vector x belonging to a certain class (G(x) in paper)
     x: input feature vector
@@ -9,4 +15,36 @@ def approximate_gradient(classifier, x, X, k, epsilon):
     k: number of principle directions to use in approximating the gradient of the classifier G(x).
     epsilon: magnitude of perturbation (must be small.)
     """
-    return lalalal
+
+    # Each row represents an observation
+    covariance_matrix = np.cov(X, rowvar=False)
+    eigenvalues, eigenvectors = eigs(covariance_matrix, k=k)
+    eigenvalues, eigenvectors = np.real(eigenvalues), np.real(eigenvectors)
+
+    classifier_x = classifier(x)
+    gradient = np.zeros(eigenvectors.shape[0])
+    for i in range(k):
+        eigenvector = eigenvectors[:, i]
+        gradient += ((classifier_x - classifier(x + epsilon*eigenvector)) / epsilon) * eigenvector
+
+    return gradient
+
+
+def visualize_gradient(gradient):
+    # reshape back to 2D array representing image.
+    gradient = np.reshape(gradient, (int(np.sqrt(gradient.shape[0])), int(np.sqrt(gradient.shape[0]))))
+    plot2d(gradient)
+
+
+def stub_classifier(x):
+    return np.random.rand()
+
+
+if __name__ == '__main__':
+    digits = datasets.load_digits()
+    feature_vectors = digits.data
+    x = feature_vectors[0]
+    k = 20
+
+    gradient = approximate_gradient(stub_classifier, x, feature_vectors, k)
+    visualize_gradient(gradient)
